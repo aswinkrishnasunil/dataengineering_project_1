7389ab19724ec3c72e152311e1fada07e4c615ee
aswinkrishna sunil <127579559+aswinkrishnasunil@users.noreply.github.com>
2023-10-03 21:06:43 +0530

    Add files via upload

--- project1.py
+++ project1.py
@@ -3,6 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
 
 #option = webdriver.ChromeOptions()
 #option.add_argument("start-maximized")
@@ -27,3 +32,38 @@ print (mylist)
 
 # Don't forget to close the WebDriver when you're done
 driver.quit()
+
+
+driver1 = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+#title1 = driver.title
+#print(title1)
+pc=[]
+op=[]
+for query in mylist:
+        driver1.get('https://finance.yahoo.com/quote/PFE?p=PFE&.tsrc=fin-srch')
+        search_element = driver1.find_element(By.XPATH,'//*[@id="yfin-usr-qry"]')
+        search_element.clear()
+        WebDriverWait(driver1, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="yfin-usr-qry"]')))
+
+        search_element.send_keys(query)
+
+        search_element.send_keys(Keys.ENTER)
+
+        
+
+        eles1=driver1.find_element(By.XPATH,'//*[@id="quote-summary"]/div[1]/table/tbody/tr[1]/td[2]')
+        a=eles1.text
+        #print("pc",query,"=" ,a)
+        eles2=driver1.find_element(By.XPATH,'//*[@id="quote-summary"]/div[1]/table/tbody/tr[2]/td[2]')
+        b=eles2.text
+        #print("op",query,"=" ,b)
+        pc.append(a)
+        op.append(b)
+        
+
+
+print(pc)  
+print(op)      
+driver1.quit()
